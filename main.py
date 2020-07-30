49c26e4c29647aeefeca93afe33dfbbe588d3c79
Max Goodman <maximgoodman@gmail.com>
2020-07-29 20:54:26 -0500

    added implementation, failing execution

--- main.py
+++
@@ -1,9 +0,0 @@
-from graphics import *
-
-if __name__ == "__main__":
-    win = GraphWin("Sup", 500, 500)
-    txt = Text(Point(250, 250), "Heeeeyyy")
-    txt.draw(win)
-
-    win.getMouse()
-    win.close()
